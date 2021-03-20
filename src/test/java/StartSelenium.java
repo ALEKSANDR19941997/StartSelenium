d2e7a94995b02d76a1c6bb2084d3bc128501da00
ALEKSANDR19941997 <80644695+ALEKSANDR19941997@users.noreply.github.com>
2021-03-20 12:01:30 +0200

    addede startSelenium

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public  void preconditions(){
+        wd =new ChromeDriver();
+    }
+
+    @Test
+    public void startGoogle(){
+        wd.get("https://www.google.co.il/");
+      //  wb.navigate().to("https://www.google.co.il/")
+
+    }
+
+
+    @AfterMethod
+    public void  tearDown(){
+       // wb.close();
+wd.quit();
+
+    }
+}
+
+
+
